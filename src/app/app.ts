355594ac5a3b73b5fb6a29ba712f9ea68175c345
minhln <minhln@abic.vn>
2025-12-17 17:46:39 +0700

    aa

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,16 @@
 import { Component, signal } from '@angular/core';
 import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { MatIconModule } from '@angular/material/icon';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatIcon, MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
-  imports: [MatSlideToggleModule, RouterOutlet, MatIconModule, RouterLinkActive, RouterLink,],
+  imports: [MatSlideToggleModule,
+    RouterOutlet,
+    MatIconModule,
+    MatIcon,
+    RouterLinkActive,
+    RouterLink,],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
--- src/app/dash-board/dash-board.ts
+++ src/app/dash-board/dash-board.ts
@@ -1,11 +1,35 @@
 import { Component } from '@angular/core';
+import {MatButtonToggle, MatButtonToggleGroup} from '@angular/material/button-toggle';
+import {MatIcon} from '@angular/material/icon';
+import {MatIconButton} from '@angular/material/button';
 
 @Component({
   selector: 'app-dash-board',
-  imports: [],
+  imports: [MatButtonToggleGroup,
+    MatButtonToggle, MatIcon, MatIconButton,],
   templateUrl: './dash-board.html',
   styleUrl: './dash-board.scss',
 })
 export class DashBoard {
+  weekDays = [
+    { name: 'Monday', number: '12' },
+    { name: 'Tuesday', number: '13' },
+    { name: 'Wednesday', number: '14' },
+    { name: 'Thursday', number: '15' },
+    { name: 'Friday', number: '16' },
+    { name: 'Saturday', number: '17' },
+    { name: 'Sunday', number: '18' }
+  ];
 
+  users = [
+    { name: 'Nguyễn Văn B' },
+    { name: 'Nguyễn Văn C' },
+    { name: 'Nguyễn Văn D' }
+  ];
+
+  getEvent(userName: string, day: string) {
+    if (userName === 'Nguyễn Văn B' && day === 'Thursday') return 'nghi-phep';
+    if (userName === 'Nguyễn Văn D' && day === 'Tuesday') return 'den-muon';
+    return null;
+  }
 }
