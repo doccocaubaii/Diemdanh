9d54356d11804f71b9d280dc5be792f5ba66f393
minhln <minhln@abic.vn>
2025-12-17 17:07:20 +0700

    aa

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,32 @@
 import { Routes } from '@angular/router';
+import {DashBoard} from './dash-board/dash-board';
+import {Member} from './member/member';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'dashboard',
+    component: DashBoard,
+    title: 'Bảng điều khiển' // Tiêu đề hiển thị trên trình duyệt
+  },
+  {
+    path: 'members',
+    component: Member,
+    title: 'Quản lý thành viên'
+  },
+  // {
+  //   path: 'reports',
+  //   component: ReportComponent,
+  //   title: 'Báo cáo hệ thống'
+  // },
+  // Đường dẫn mặc định khi vừa mở app
+  {
+    path: '',
+    redirectTo: '/dashboard',
+    pathMatch: 'full'
+  },
+  // Trang 404 nếu người dùng gõ sai URL (nên có)
+  {
+    path: '**',
+    redirectTo: '/dashboard'
+  }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,11 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
-  imports: [MatSlideToggleModule, RouterOutlet,MatIconModule,],
+  imports: [MatSlideToggleModule, RouterOutlet, MatIconModule, RouterLinkActive, RouterLink,],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
